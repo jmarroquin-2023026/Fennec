a4d28fd1a4c18c72293d3dec3f4d2d1de66be9cc
dmedina-2023045 <dmedina-2023045@kinal.edu.gt>
2024-07-08 21:44:05 -0600

    Se agrega la entidad Proveedores y atributos

---
+++ DB_Fennec.sql
@@ -0,0 +1,34 @@
+drop database if exists fennec;
+create database Fennec;
+use  fennec;
+ 
+ 
+create table Carro (
+    carroId int not null auto_increment,
+    VIN int not null,
+    marca VARCHAR(50) not null,
+    modelo VARCHAR(50) not null,
+    año int not null,
+    precio DECIMAL(10, 2) not null,
+	primary key PK_carroId (carroId)
+);
+
+create table Cliente (
+    clienteID INT PRIMARY KEY not null,
+    nombre VARCHAR(50) not null,
+    apellido VARCHAR(50)not null,
+    airección VARCHAR(100) not null,
+    teléfono VARCHAR(20)not null,
+    correo varchar(50) not null
+);
+
+create table Proveedor(
+    codigoProveedor int not null auto_increment,
+    nombresProveedor varchar(50) not null,
+    apellidosProveedor varchar(50) not null,
+    direccionProveedor varchar(200) not null,
+    telefonoProveedor varchar(8) not null,
+    correoProveedor varchar(50) not null,
+    observacionesProveedor varchar(150) not null,
+    Primary key PK_CodigoProveedor (codigoProveedor)
+);
